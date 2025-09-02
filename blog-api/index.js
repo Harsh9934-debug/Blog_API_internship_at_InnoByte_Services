56170891b41814fa74e55b995fd8a28bc891f3e5
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-09-03 00:41:00 +0530

    updated the mongoose file

--- blog-api/index.js
+++ blog-api/index.js
@@ -1,6 +1,12 @@
 const express = require ("express")
 const app = express();
+const dotenv = require("dotenv")
+const mongoose =  require ("mongoose")
+dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB connected "))
+    .catch((err) => console.error("MongoDB connection error ", err));
 
 app.listen("5000",()=>{
     console.log ("Backend is running")
