beb86bf5c38ae3cabf17666be3bb987cd8482828
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-09-06 19:40:29 +0530

    updated the scheme model of the blog_api

---
+++ blog-api/models/comment.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+    {
+        id: {
+            type: String,
+            required: true,
+        },
+        postId: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        author_id: {
+            type: String,
+            required: true,
+        },
+        created_at: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("comment",commentSchema);
