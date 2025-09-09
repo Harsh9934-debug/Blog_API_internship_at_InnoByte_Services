a9587b3d5c313c7b1638cb8c55297ad0df2638f0
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-09-10 00:27:59 +0530

    Createed the categories

--- blog-api/index.js
+++ blog-api/index.js
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
 import userRoute from "./routes/users.js";
 import postRoute from "./routes/post.js";
+import categoriesRoute from "./routes/categories.js";
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -17,6 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use("/api/auth",authRoute);
 app.use("/api/user",userRoute);
 app.use("/api/post",postRoute);
+app.use("/api/post",categoriesRoute);
 
 
 app.listen("5000",()=>{
