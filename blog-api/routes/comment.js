b4024d93639a5ed1dd3a3d580b4814df981be33a
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-09-11 21:35:16 +0530

    Created the try catch block

--- blog-api/routes/comment.js
+++ blog-api/routes/comment.js
@@ -1,3 +1,11 @@
 const express = require("express")
 const router = express.Router();
 const comment = require("../models/comment")
+
+router.post("/",async (req, res) =>{
+    try{
+        
+    }catch(err){
+        res.status(500).json("Error occured")
+    }
+})
